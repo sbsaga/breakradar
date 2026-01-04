21b6ce4dce6520f3d33d8c6595c069a054c0ad65
Sagar Sunil Bhedodkar <sagarbhedodkar456@gmail.com>
2026-01-04 21:20:29 +0530

    fix: finalize BreakRadar verbosity and reporter

--- src/Command/CheckCommand.php
+++ src/Command/CheckCommand.php
@@ -20,30 +20,33 @@ final class CheckCommand extends Command
 {
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $reporter = new ConsoleReporter($output);
+
         $git = new GitRepository(new GitRunner());
         $git->ensureClean();
 
-        $storage = new SnapshotStorage();
+        $storage  = new SnapshotStorage();
         $analyzer = new PublicApiAnalyzer();
-        $diff = new BreakingChangeDiff();
-        $reporter = new ConsoleReporter();
+        $diff     = new BreakingChangeDiff();
 
         $baseRef = $git->defaultRemoteBranch();
 
         try {
+            $reporter->info("Fetching base branch: {$baseRef}");
             $git->fetch($baseRef);
 
+            $reporter->debug("Snapshotting base branch");
             $git->checkout($baseRef);
             $base = $analyzer->analyze(getcwd() . '/src', 'BreakRadar\\');
             $storage->write('base', $base);
 
+            $reporter->debug("Snapshotting current branch");
             $git->restore();
             $head = $analyzer->analyze(getcwd() . '/src', 'BreakRadar\\');
             $storage->write('head', $head);
 
             return $reporter->report(
-                $diff->diff($base, $head),
-                $output
+                $diff->diff($base, $head)
             );
         } finally {
             $git->restore();
--- src/Reporter/ConsoleReporter.php
+++ src/Reporter/ConsoleReporter.php
@@ -6,16 +6,40 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 final class ConsoleReporter
 {
-    public function report(array $issues, OutputInterface $output): int
+    public function __construct(
+        private OutputInterface $output
+    ) {}
+
+    public function info(string $message): void
     {
-        if (empty($issues)) {
-            $output->writeln('<info>No breaking changes detected.</info>');
+        $this->output->writeln($message, OutputInterface::VERBOSITY_VERBOSE);
+    }
+
+    public function debug(string $message): void
+    {
+        $this->output->writeln($message, OutputInterface::VERBOSITY_VERY_VERBOSE);
+    }
+
+    public function trace(string $message): void
+    {
+        $this->output->writeln($message, OutputInterface::VERBOSITY_DEBUG);
+    }
+
+    public function report(array $breakingChanges): int
+    {
+        if (empty($breakingChanges)) {
+            $this->output->writeln(
+                '<info>No breaking changes detected.</info>'
+            );
             return 0;
         }
 
-        $output->writeln('<error>Breaking changes detected:</error>');
-        foreach ($issues as $issue) {
-            $output->writeln(" - {$issue}");
+        $this->output->writeln(
+            '<error>Breaking changes detected:</error>'
+        );
+
+        foreach ($breakingChanges as $change) {
+            $this->output->writeln(" - {$change}");
         }
 
         return 1;
