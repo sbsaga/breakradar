e954245aa5d3b9da7c5fca16a34a2e8ca266b611
Sagar Sunil Bhedodkar <sagarbhedodkar456@gmail.com>
2026-01-04 21:36:45 +0530

    test: remove public method (breaking)

--- src/Analyzer/PublicApiAnalyzer.php
+++ src/Analyzer/PublicApiAnalyzer.php
@@ -7,38 +7,38 @@ use ReflectionMethod;
 
 final class PublicApiAnalyzer
 {
-    public function analyze(string $srcDir, string $namespacePrefix = ''): array
-    {
-        $result = [];
+    // public function analyze(string $srcDir, string $namespacePrefix = ''): array
+    // {
+    //     $result = [];
 
-        foreach ($this->phpFiles($srcDir) as $file) {
-            require_once $file;
-        }
+    //     foreach ($this->phpFiles($srcDir) as $file) {
+    //         require_once $file;
+    //     }
 
-        foreach (get_declared_classes() as $class) {
-            if ($namespacePrefix && !str_starts_with($class, $namespacePrefix)) {
-                continue;
-            }
+    //     foreach (get_declared_classes() as $class) {
+    //         if ($namespacePrefix && !str_starts_with($class, $namespacePrefix)) {
+    //             continue;
+    //         }
 
-            $ref = new ReflectionClass($class);
+    //         $ref = new ReflectionClass($class);
 
-            if (!$ref->isUserDefined()) {
-                continue;
-            }
+    //         if (!$ref->isUserDefined()) {
+    //             continue;
+    //         }
 
-            foreach ($ref->getMethods(ReflectionMethod::IS_PUBLIC) as $method) {
-                if ($method->isConstructor()) continue;
+    //         foreach ($ref->getMethods(ReflectionMethod::IS_PUBLIC) as $method) {
+    //             if ($method->isConstructor()) continue;
 
-                $result[$ref->getName()][] = [
-                    'method' => $method->getName(),
-                    'params' => array_map(fn($p) => $p->getName(), $method->getParameters()),
-                ];
-            }
-        }
+    //             $result[$ref->getName()][] = [
+    //                 'method' => $method->getName(),
+    //                 'params' => array_map(fn($p) => $p->getName(), $method->getParameters()),
+    //             ];
+    //         }
+    //     }
 
-        ksort($result);
-        return $result;
-    }
+    //     ksort($result);
+    //     return $result;
+    // }
 
     private function phpFiles(string $dir): array
     {
